5eb9822588c3066761606258ebe87f2ce4e01737
SeafoodBibimbap <155558935+SeafoodBibimbap@users.noreply.github.com>
2024-03-11 18:23:24 +0900

    static 경로 추가

--- uPC_project/uPC_project/settings.py
+++ uPC_project/uPC_project/settings.py
@@ -139,6 +139,10 @@ SITE_ID = 1
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
