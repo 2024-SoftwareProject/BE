5c1cfbade62e68bc6babb20cc590c5b9493967f8
hannah0352 <105264420+hannah0352@users.noreply.github.com>
2024-03-11 18:32:32 +0900

    주석 해제

--- uPC_project/products_app/urls.py
+++ uPC_project/products_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-# app_name = 'products_app'
+app_name = 'products_app'
 
 urlpatterns = [
     # path("", views.test, name="test"),
-    path("", views.search_view, name='search'),
+    path('', views.search_view, name='search'),
 ]
