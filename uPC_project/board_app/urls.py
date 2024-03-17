a23f8df0136e526eff822860b44051b71ce2e547
ijyeon22 <kellylee0714@gmail.com>
2024-03-17 17:01:59 +0900

    게시판_수정사항

--- uPC_project/board_app/urls.py
+++ uPC_project/board_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth.views import LoginView
 
 app_name='board_app'
 
--- uPC_project/private_settings.py
+++ uPC_project/private_settings.py
@@ -3,9 +3,9 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'upc_db', # mydatabase
+        'NAME': 'mydb', # mydatabase
         'USER': 'root', # mydatabaseuser
-        'PASSWORD': '0309@dlfroal', # mypassword
+        'PASSWORD': 'thdnp!1004', # mypassword
         'HOST': 'localhost', # host
         'PORT': '3306',
     }
