4541be2d5ae076e63efe6cacf23e07bec1ea31f2
GreatJung1 <andelion0117@naver.com>
2024-03-25 03:11:31 +0900

    메인페이지1' ;

---
+++ uPC_project/uPC_app/home_get_data.py
@@ -0,0 +1,13 @@
+from products_app.models import Product
+from board_app.models import Post
+from django.db import models
+
+def get_products_by_popularity():
+    products = Product.objects.order_by('-Pd_Count')[:5]
+    return products
+
+def get_posts():
+    posts = posts = Post.objects.all().order_by('-id')[:5]
+
+    return posts
+    
--- uPC_project/uPC_app/views.py
+++ uPC_project/uPC_app/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from products_app.models import Product
+from board_app.models import Post
+from . import home_get_data
 
 def home(request):
-    return render(request, 'home.html')
+
+    popularity_outputDB = home_get_data.get_products_by_popularity()
+    post_outputDB = home_get_data.get_posts()
+
+
+    
+    return render(request, 'home.html', {'popularity_outputDB':popularity_outputDB , 'post_outputDB': post_outputDB})
+
+
