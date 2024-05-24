a03b5ad6a274119814684d233dc6ded67ea23337
이정원 <jyang2790@naver.com>
2024-05-24 15:43:56 +0900

    commit

--- uPC_project/private_settings.py
+++ uPC_project/private_settings.py
@@ -3,9 +3,9 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'upc_db', # mydatabase
+        'NAME': 'upc_db2', # mydatabase
         'USER': 'root', # mydatabaseuser
-        'PASSWORD': 'juwon1106!', # mypassword
+        'PASSWORD': 'soft1234', # mypassword
         'HOST': 'localhost', # host
         'PORT': '3306',
     }
